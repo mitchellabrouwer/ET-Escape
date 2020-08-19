720b14b528fff466ee0d8fd4d9299cb274da55dc
Mitch <mitch@Mitchs-iMac.local>
2020-08-19 22:34:32 +1000

    fix: player now correctly moving around board

--- src/controller/game.js
+++ src/controller/game.js
@@ -8,19 +8,21 @@ const right = (event) => event.keyCode === 39 || event.keyCode === 68
 const down = (event) => event.keyCode === 40 || event.keyCode === 83
 
 export function updateGame(event) {
-  const level = document.querySelectorAll('.grid-item')
-  const prevNode = document.querySelector('.player-image.active')
-  const currentIndex = [...level].indexOf(prevNode.parentNode)
+  const squares = document.querySelectorAll('.player-image')
+  const playerOn = document.querySelector('.player-image.active')
   const height = maps[currentLevel].length
   const width = maps[currentLevel][0].length
+  const index = [...squares].indexOf(playerOn)
+  const row = Math.floor(index / height)
+  const column = index % width
 
   if (left(event)) {
-    moveHorizontal(prevNode, currentIndex - 1, width, level)
+    moveHorizontal(playerOn, [row, column - 1], width, squares)
   } else if (right(event)) {
-    moveHorizontal(prevNode, currentIndex + 1, width, level)
+    moveHorizontal(playerOn, [row, column + 1], width, squares)
   } else if (up(event)) {
-    moveVertical(prevNode, currentIndex - width, width, height, level)
+    moveVertical(playerOn, [row - 1, column], width, height, squares)
   } else if (down(event)) {
-    moveVertical(prevNode, currentIndex + width, width, height, level)
+    moveVertical(playerOn, [row + 1, column], width, height, squares)
   }
 }
