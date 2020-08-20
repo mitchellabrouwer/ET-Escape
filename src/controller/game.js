02baac2a750c71820f0dcfbf8080a2ddff20bad7
Mitch <mitch@Mitchs-iMac.local>
2020-08-20 11:48:21 +1000

    refactor: clean up move functionality again

--- src/controller/game.js
+++ src/controller/game.js
@@ -3,33 +3,42 @@ import { level } from '../model/state'
 import PuzzleDisplay from '../view/puzzle'
 
 const left = (event) => event.keyCode === 37 || event.keyCode === 65
-const up = (event) => event.keyCode === 38 || event.keyCode === 67
+const up = (event) => event.keyCode === 38 || event.keyCode === 87
 const right = (event) => event.keyCode === 39 || event.keyCode === 68
 const down = (event) => event.keyCode === 40 || event.keyCode === 83
+const isMove = (event) => [left, up, right, down].some((fn) => fn(event))
 
-function move(currentNode, index, [rowMove, columnMove], squares) {
+function moveTo(index, [rowMove, columnMove]) {
   const height = levels[level].map.length
   const width = levels[level].map[0].length
   const newRow = (Math.floor(index / height) + rowMove + width) % width
   const newColumn = ((index % width) + columnMove + height) % height
-  const newNode = squares[newRow * width + newColumn]
+  const newIndex = newRow * width + newColumn
 
-  PuzzleDisplay.movePlayer(currentNode, newNode)
+  return newIndex
+}
+
+function movePlayer(event) {
+  const squares = document.querySelectorAll('.player-image')
+  const playerOn = document.querySelector('.player-image.active')
+  const currentIndex = [...squares].indexOf(playerOn)
+  let newIndex
+
+  if (left(event)) {
+    newIndex = moveTo(currentIndex, [0, -1])
+  } else if (right(event)) {
+    newIndex = moveTo(currentIndex, [0, 1])
+  } else if (up(event)) {
+    newIndex = moveTo(currentIndex, [-1, 0])
+  } else if (down(event)) {
+    newIndex = moveTo(currentIndex, [1, 0])
+  }
+
+  PuzzleDisplay.movePlayer(playerOn, squares[newIndex])
+
+  return newIndex
 }
 
 export function updateGame(event) {
-  const squares = document.querySelectorAll('.player-image')
-  const playerOn = document.querySelector('.player-image.active')
-
-  const index = [...squares].indexOf(playerOn)
-
-  if (left(event)) {
-    move(playerOn, index, [0, -1], squares)
-  } else if (right(event)) {
-    move(playerOn, index, [0, 1], squares)
-  } else if (up(event)) {
-    move(playerOn, index, [-1, 0], squares)
-  } else if (down(event)) {
-    move(playerOn, index, [+1, 0], squares)
-  }
+  if (isMove(event)) movePlayer(event)
 }
